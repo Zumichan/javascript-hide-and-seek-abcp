c986d017b18a1f8cc7be946b55c04869de463dd6
Azumi Callanan <azucm@hotmail.com>
2018-01-16 19:06:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function nestedTarget(){
 
 function increaseRankBy(n){
   var rank = document.querySelectorAll('.ranked-list');
-  for(var i=0;i<ranks.length;i++){
+  for(var i=0;i<rank.length;i++){
     rank[i].innerHTML = parselnt(rank[i].innerHTML)+n;
   }
   return rank;
